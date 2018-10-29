38a1b4aaf2e9c15f1c5a73960d31be8147ee2d44
Tong Guo <779222056@qq.com>
2018-10-29 12:16:16 +0800

    Update position_embedding.py

--- position_embedding.py
+++ position_embedding.py
@@ -1,6 +1,6 @@
   # edit the SymbolModality's bottom_simple of https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/layers/modalities.py
   
-  def position_embedding(self,x):
+  def position_embedding(self,ret):
       pe = np.zeros(shape=[100,128])
       position = np.expand_dims(np.arange(0,100),-1)
       div_term = np.arange(0, 128, 2) * (-math.exp(math.log(10000.0) / 128))
@@ -9,7 +9,7 @@
       pe[:,0::2]=sin_
       pe[:,1::2]=cos_
       pe = tf.expand_dims(tf.constant(value=pe),0)
-      pe = pe[:, :tf.shape(x)[1]]
+      pe = pe[:, :tf.shape(ret)[1]]
       pe = tf.cast(tf.expand_dims(pe, -2), tf.float32)
-      ret = x+pe
+      ret = ret + pe
       return ret
